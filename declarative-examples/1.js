c6b8b5f30d131358162ccc12b9e88b021dc0530d
Carolina Ale <redrumjedi@gmail.com>
2021-02-28 22:52:16 -0300

    add second example

--- declarative-examples/1.js
+++ declarative-examples/1.js
@@ -1,4 +1,4 @@
-//Calculating Avg Points
+//1) Calculating Avg Points
 
 const points = [1, 3, 5, 7, 8, 3, 12, 88, 11, 5, 34, 3, 2];
 
@@ -6,3 +6,18 @@ const sum = (a, b) => a + b;
 const division = (a, b) => a / b;
 
 const avgPoints = divide(points.reduce(sum), points.length);
+
+//2) Filtering who has the most points
+
+const people = [
+  { firstName: "person1", points: 3 },
+  { firstName: "person2", points: 5 },
+  { firstName: "person3", points: 5 },
+  { firstName: "person4", points: 7 },
+  { firstName: "person5", points: 9 },
+  { firstName: "person6", points: 2 },
+  { firstName: "person7", points: 2 },
+];
+
+const filterPoints = (person) => person.nota >= 7;
+const approved = people.filter(filterPoints);
